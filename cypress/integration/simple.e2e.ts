ed54230c704f106a465edec23e59eace49416c63
Huang Jian <huang.jian@howtimeflies.io>
2019-03-31 10:25:49 +0800

    introduce typescript.

--- cypress/integration/simple.e2e.ts
+++ cypress/integration/simple.e2e.ts
@@ -1,7 +1,7 @@
 describe('My First Test', function() {
     it('Does not do much!', function() {
         cy.wrap({})
-        expect(true).to.equal(true)
+        // expect(true).to.equal(true)
         expect(true).toEqual(true)
         expect(function() {throw 'error'}).toThrow()
         expect(function() {throw new TypeError()}).toThrow(TypeError)
