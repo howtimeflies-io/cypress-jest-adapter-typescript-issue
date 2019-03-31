59286b34327bce0e114246486121bf8fcf21438d
Huang Jian <huang.jian@howtimeflies.io>
2019-03-31 10:05:09 +0800

    introduce cypress-jest-adapter.

---
+++ cypress/integration/simple.e2e.js
@@ -0,0 +1,14 @@
+describe('My First Test', function() {
+    it('Does not do much!', function() {
+        cy.wrap({})
+        expect(true).to.equal(true)
+        expect(true).toEqual(true)
+        expect(function() {throw 'error'}).toThrow()
+        expect(function() {throw new TypeError()}).toThrow(TypeError)
+        expect(function() {}).toThrow()
+    })
+    //
+    // it('should open the home page', () => {
+    //     cy.visit('localhost:3000')
+    // })
+})
